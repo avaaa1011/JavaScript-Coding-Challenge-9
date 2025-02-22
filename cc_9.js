d126d3f220e7d67d9ab747d2c18c4ad9ef40dcb0
Ava Tran <ava31@usf.edu>
2025-02-21 23:30:18 -0500

    Task 4: implemented payroll system

--- cc_9.js
+++ cc_9.js
@@ -38,16 +38,20 @@ class Manager extends Employee //new class but it extends the employee class fro
             return(`Manager Name: ${this.name}\n Employee ID: ${this.id}\n Department: ${this.department}\n Salary: $${this.salary}\n Team Size: ${this.teamSize}\n`);
         }
         calculateBonus() //adding a new method that will return 10% of a manager's annual salary
+        {                
+            return super.calculateAnnualSalary() * 0.1;
+        };
+        calculateAnnualSalary() //modifed this method so it can now consider bonuses for managers for task 4
         {
-            const bonus = 0.1
-            const startingSalary = this.salary * 12;
-            return bonus * startingSalary;
-        }
+            return super.calculateAnnualSalary() + this.calculateBonus(); 
+        };
+    }
         const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
         console.log(mgr1.getDetails());
         // Expected output: "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
         console.log(mgr1.calculateBonus()); 
         // Expected output: 9600
+    
     
 //Task 3: Creating a Company Class
 class Company //created a new class
@@ -65,6 +69,10 @@ class Company //created a new class
         {
             this.employees.forEach(employee => {console.log(employee.getDetails())});
         }
+        calculateTotalPayroll() // adding a method to the Company class for Task 4
+        { //method will return the sum of employees and managers salaries 
+            return this.employees.reduce((total, employee) => total + employee.calculateAnnualSalary(), 0); 
+        }
     }
     const company = new Company("TechCorp");
     company.addEmployee(emp1);
@@ -73,6 +81,13 @@ class Company //created a new class
     // Expected output:
     // "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
     // "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
+
+//Task 4: implementing a payroll system
+console.log(company.calculateTotalPayroll()); 
+// Expected output: 165600 (assuming emp1 and mgr1 salaries)
+
+
+
 
 
 
